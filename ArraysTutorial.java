2c8c1eb4d5089d05539d8429f99e7d62e93540b3
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-07-12 21:43:13 +0530

    completed concept of loops and arrays

---
+++ ArraysTutorial.java
@@ -0,0 +1,16 @@
+ArraysTutorial {
+    public static void main(String[] args) {
+        /*int[] arr = new int[5];
+        arr[0] = 12;
+        arr[1] = 24;
+        arr[2] = 10;
+        arr[3] = 14;
+        arr[4] = 98;*/
+
+        int[] arr = {12,24,10,14,98};
+
+        for(int i=0; i<arr.length; i++) {
+            System.out.println("Value of arr[i] is " + arr[i]);
+        }
+    }
+}
---
+++ LoopExample.java
@@ -0,0 +1,17 @@
+LoopsTutorial {
+    public static void main(String[] args) {
+        /*for(int i=1; i<=0; i++) {
+            System.out.println("Value of i is " + i);
+        }*/
+        /*int i=1;
+        while(i<=0) {
+            System.out.println("Value of i is " + i);
+            i = i+3;
+        }*/
+        int i=1;
+        do {
+            System.out.println("Value of i is " + i);
+            i++;
+        } while(i<=0);
+    }
+}
