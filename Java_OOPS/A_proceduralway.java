03fb22e8e1441bffac4543ea79671c9d8a8a9768
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-07-17 18:45:24 +0530

    procedural way in java

---
+++ Java_OOPS/A_proceduralway.java
@@ -0,0 +1,12 @@
+package Java_OOPS;
+
+public class A_proceduralway {
+    static float radius = 10.0f;
+    public static void main(String[] args) {
+        System.out.println(area(radius));
+    }
+
+    public static float area(float radius) {
+        return 3.14f * radius * radius;
+    }
+}
