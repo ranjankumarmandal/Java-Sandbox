379bada0be7b86e79ed8f30737e746fac028cef5
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-07-17 21:40:50 +0530

    completed the concepts - why to use more than one class in OOPs

--- Java_OOPS/A1_why_more_than_one_class.java
+++ Java_OOPS/A1_why_more_than_one_class.java
@@ -3,18 +3,19 @@ package Java_OOPS;
 public class A1_why_more_than_one_class {
     static float radius = 10.0f;
     public static void main(String[] args) {
-        System.out.println()
+        System.out.println(Circle.area(radius));
+        System.out.println(Square.area(radius));
     }
 }
 
 class Circle {
-    float area(float radius) {
+    static float area(float radius) {
         return 3.14f * radius * radius;
     }
 }
 
 class Square {
-    float area(float side) {
+    static float area(float side) {
         return side * side;
     }
 }
