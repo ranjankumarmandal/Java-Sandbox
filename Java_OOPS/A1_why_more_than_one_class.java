5c23618c9aad2f98f61a4b18e02be244b3a26feb
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-07-17 21:37:40 +0530

    working on concept of oops - why we need more than one class

---
+++ Java_OOPS/A1_why_more_than_one_class.java
@@ -0,0 +1,20 @@
+package Java_OOPS;
+
+public class A1_why_more_than_one_class {
+    static float radius = 10.0f;
+    public static void main(String[] args) {
+        System.out.println()
+    }
+}
+
+class Circle {
+    float area(float radius) {
+        return 3.14f * radius * radius;
+    }
+}
+
+class Square {
+    float area(float side) {
+        return side * side;
+    }
+}
