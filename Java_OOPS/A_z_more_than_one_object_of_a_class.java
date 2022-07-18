2fa65a0830cffaff65634546537c6424bbc38fc9
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-07-18 21:05:52 +0530

    added getter methods in class

--- Java_OOPS/A_z_more_than_one_object_of_a_class.java
+++ Java_OOPS/A_z_more_than_one_object_of_a_class.java
@@ -1,4 +1,19 @@
 package Java_OOPS;
 
 public class A_z_more_than_one_object_of_a_class {
+    public int roll;
+    public String name;
+    public double marks;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getRoll() {
+        return roll;
+    }
+
+    public double getMarks() {
+        return marks;
+    }
 }
