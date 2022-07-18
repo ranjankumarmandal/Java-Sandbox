5a6cc3e0f01a1869802edc5017de1651435264ec
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-07-19 01:22:00 +0530

    completed the implmentation of constructors in java - passing value to constructors

--- Java_OOPS/a_z1_constructors.java
+++ Java_OOPS/a_z1_constructors.java
@@ -2,13 +2,21 @@ package Java_OOPS;
 
 public class a_z1_constructors {
     public static void main(String[] args) {
+        Circle c1 = new Circle(12.1f);
+        System.out.println("area is " + c1.area());
 
+        Circle c2 = new Circle(26.1f);
+        System.out.println("area is " + c2.area());
     }
 }
 
 class Circle {
     public float radius;
-    public Circle(radius) {
+    public Circle(float radius) {
         this.radius = radius;
+    }
+
+    public float area() {
+        return 3.14f * radius * radius;
     }
 }
