c06705f05c285d7281ef4eff4b82f33d280c0b77
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-07-19 01:04:17 +0530

    completed more than one object implementation for a given class

--- Java_OOPS/A_z_more_than_one_object_of_a_class.java
+++ Java_OOPS/A_z_more_than_one_object_of_a_class.java
@@ -1,19 +1,20 @@
 package Java_OOPS;
 
+class Circle {
+    public float area(float radius) {
+        return 3.14f * radius;
+    }
+}
+
 public class A_z_more_than_one_object_of_a_class {
-    public int roll;
-    public String name;
-    public double marks;
+   public static void main(String[] args) {
+       Circle c1 = new Circle();
+       System.out.println("first area" + c1.area(2.1f));
 
-    public String getName() {
-        return name;
-    }
+       Circle c2 = new Circle();
+       System.out.println("second area" + c2.area(10f));
 
-    public int getRoll() {
-        return roll;
-    }
-
-    public double getMarks() {
-        return marks;
-    }
+       Circle c3 = new Circle();
+       System.out.println("third aread" + c3.area(15.2f));
+   }
 }
