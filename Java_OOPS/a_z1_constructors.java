f68e997302716acb18ca7421991c79c0669632a0
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-07-19 01:17:28 +0530

    created a class Circle with constructor inside it - constructors has same name in java as thier class name

--- Java_OOPS/a_z1_constructors.java
+++ Java_OOPS/a_z1_constructors.java
@@ -2,6 +2,13 @@ package Java_OOPS;
 
 public class a_z1_constructors {
     public static void main(String[] args) {
-        
+
+    }
+}
+
+class Circle {
+    public float radius;
+    public Circle(radius) {
+        this.radius = radius;
     }
 }
