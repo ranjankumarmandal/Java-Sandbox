c253c61b1872e931bb1c6864804582a2d5cee2c0
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-07-21 00:56:58 +0530

    completed private access modifier implementation in java|

--- Java_OOPS/A5_private_access_modifiers.java
+++ Java_OOPS/A5_private_access_modifiers.java
@@ -1,4 +1,21 @@
 package Java_OOPS;
 
 public class A5_private_access_modifiers {
+    public static void main(String[] args) {
+        Circle5 c1 = new Circle5(4.5f);
+        c1.area();
+        // here we can't do c1.radius, because radius inside class Circle5 is private
+    }
+}
+
+class Circle5 {
+    private float radius;
+
+    Circle5(float radius) {
+        this.radius = radius;
+    }
+
+    public float area() {
+        return 3.14f * this.radius * this.radius;
+    }
 }
