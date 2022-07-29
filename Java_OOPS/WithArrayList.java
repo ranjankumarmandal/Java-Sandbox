8bc1c02b37642f55dfbc3f0cfa7a4ed81ef73849
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-07-29 23:41:30 +0530

    initialized arraylist

--- Java_OOPS/WithArrayList.java
+++ Java_OOPS/WithArrayList.java
@@ -3,5 +3,12 @@ import java.util.*;
 public class WithArrayList {
 
   public static void main (String[] args) {
+    ArrayList studentList = new ArrayList();
+
+    studentList.add(new Student("Sujit", 1));
+    studentList.add(new Student("Siddharth", 2));
+    studentList.add(new Student("Karanpreet", 3));
+    
+    printStudentList(studentList);
   }
 }
